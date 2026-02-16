e6294ea89cf00271bebe24f8ba5e3306f4f8f72d
Liz Burke <liz.burke.code@gmail.com>
2026-02-16 11:37:38 -0500

    update + delete

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const Product = require('./models/productModel');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.get('/', (req, res) => {
@@ -15,6 +16,7 @@ app.get('/blog', (req, res) => {
   res.send('Hello Blog Route..');
 })
 
+// get all products
 app.get('/products', async (req, res) => {
   try {
     const products = await Product.find({});
@@ -25,6 +27,7 @@ app.get('/products', async (req, res) => {
   }
 });
 
+// get a single product by ID
 app.get('/products/:id', async (req, res) => {
   try {
     const { id } = req.params;
@@ -39,7 +42,8 @@ app.get('/products/:id', async (req, res) => {
   }
 });
 
-app.post('/product', async (req, res) => {
+// create a new product
+app.post('/products', async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
@@ -49,6 +53,37 @@ app.post('/product', async (req, res) => {
     res.status(500).json({message: error.message});
   }
 })
+
+// update a product by ID
+app.put('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body)
+    if (!product) {
+      return res.status(404).json({message: `cannot find product with ID ${id}`});
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({message: error.message});
+  }
+});
+
+// delete a product by ID
+app.delete('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({message: `cannot find product with ID ${id}`});
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({message: error.message});
+  }
+});
 
 mongoose.connect('mongodb+srv://admin:aXiLKyZEldlZYnD9@nodeprojectapi.oipumkw.mongodb.net/?appName=NodeProjectAPI')
 .then(() => {
