623af2a60a84dee8e528014b1e7aefebd26ff67e
Liz Burke <liz.burke.code@gmail.com>
2026-02-16 10:18:42 -0500

    create model

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // routes
@@ -7,6 +8,16 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-app.listen(3000, () => {
-  console.log('node api is running on port 3000');
+app.get('/blog', (req, res) => {
+  res.send('Hello Blog Route..');
+})
+
+mongoose.connect('mongodb+srv://admin:aXiLKyZEldlZYnD9@nodeprojectapi.oipumkw.mongodb.net/?appName=NodeProjectAPI')
+.then(() => {
+    console.log('connected to MongoDB');
+    app.listen(3000, () => {
+        console.log('node_api is running on port 3000');
+});
+}).catch((err) => {
+  console.log('error connecting to MongoDB', err);
 });
