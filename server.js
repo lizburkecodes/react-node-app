49d3f2be85bda91f6bf691b4bf85d73a0b9543e3
Liz Burke <liz.burke.code@gmail.com>
2026-02-16 11:09:10 -0500

    post + get database routes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/productModel');
 const app = express();
+
+app.use(express.json());
 
 // routes
 app.get('/', (req, res) => {
@@ -10,6 +13,41 @@ app.get('/', (req, res) => {
 
 app.get('/blog', (req, res) => {
   res.send('Hello Blog Route..');
+})
+
+app.get('/products', async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+     console.error(error.message);
+    res.status(500).json({message: error.message});
+  }
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({message: `cannot find product with ID ${id}`});
+    }
+    res.status(200).json(product);
+  } catch (error) {
+     console.error(error.message);
+    res.status(500).json({message: error.message});
+  }
+});
+
+app.post('/product', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({message: error.message});
+  }
 })
 
 mongoose.connect('mongodb+srv://admin:aXiLKyZEldlZYnD9@nodeprojectapi.oipumkw.mongodb.net/?appName=NodeProjectAPI')
